fcbad9fcc3c1266baa8d0f18d5d5a5e05fda31c1
ningdandan <einooo520@gmail.com>
2025-04-30 13:38:08 -0700

    Initial Commit

---
+++ imagineTests/imagineTests.swift
@@ -0,0 +1,17 @@
+//
+//  imagineTests.swift
+//  imagineTests
+//
+//  Created by Ningdan Zhang on 4/30/25.
+//
+
+import Testing
+@testable import imagine
+
+struct imagineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
